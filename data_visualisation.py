c3b0c9ae73b6669186c61891aaa57b91b96227b1
harshit987 <hakumar@iitk.ac.in>
2019-05-27 02:14:17 +0530

    second_commit

--- data_visualisation.py
+++ data_visualisation.py
@@ -42,3 +42,51 @@ print("Percentage of male that survived:", round(male_survived), "%")
 from IPython.display import display
 
 display(df_sex)
+#-------------------- Survived/Died by Embarked ----------------------------
+
+survived_embark = dataset[dataset['Survived']==1]['Embarked'].value_counts()
+dead_embark = dataset[dataset['Survived']==0]['Embarked'].value_counts()
+df_embark = pd.DataFrame([survived_embark,dead_embark])
+df_embark.index = ['Survived','Died']
+df_embark.plot(kind='bar',stacked=True, figsize=(5,3))
+plt.show(10)
+Embark_S= df_embark.iloc[0,0]/df_embark.iloc[:,0].sum()*100
+Embark_C = df_embark.iloc[0,1]/df_embark.iloc[:,1].sum()*100
+Embark_Q = df_embark.iloc[0,2]/df_embark.iloc[:,2].sum()*100
+print("Percentage of Embark S that survived:", round(Embark_S), "%")
+print("Percentage of Embark C that survived:" ,round(Embark_C), "%")
+print("Percentage of Embark Q that survived:" ,round(Embark_Q), "%")
+
+from IPython.display import display
+display(df_embark)
+#----------feauture preprocessing-------------------------------
+X = dataset.drop(['PassengerId','Cabin','Ticket','Fare', 'Parch', 'SibSp'], axis=1)
+y = X.Survived                       # vector of labels (dependent variable)
+X=X.drop(['Survived'], axis=1)       # remove the dependent variable from the dataframe X
+
+X.head(20)
+
+# ----------------- Encoding categorical data -------------------------
+
+# encode "Sex"
+from sklearn.preprocessing import LabelEncoder
+labelEncoder_X = LabelEncoder()
+X.Sex=labelEncoder_X.fit_transform(X.Sex)
+
+
+# encode "Embarked"
+
+# number of null values in embarked:
+print ('Number of null values in Embarked:', sum(X.Embarked.isnull()))
+
+# fill the two values with one of the options (S, C or Q)
+row_index = X.Embarked.isnull()
+X.loc[row_index,'Embarked']='S'
+
+Embarked  = pd.get_dummies(  X.Embarked , prefix='Embarked'  )
+X = X.drop(['Embarked'], axis=1)
+X= pd.concat([X, Embarked], axis=1)
+# we should drop one of the columns
+X = X.drop(['Embarked_S'], axis=1)
+
+X.head()
